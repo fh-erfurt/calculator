a271ed9e364205fc08303dd56d67c33f7dac7184
li3176me <lisa.meszaros@fh-erfurt.de>
2018-07-06 19:26:00 +0200

    made all calculations

--- src/main/java/de/fherfurt/git/Calculator.java
+++ src/main/java/de/fherfurt/git/Calculator.java
@@ -2,4 +2,19 @@ package de.fherfurt.git;
 
 public class Calculator {
 
+    public int plus(int i, int i1) {
+        return i + i1;
+    }
+
+    public int minus(int a, int b) {
+        return a-b;
+    }
+
+    public int mal(int c, int d) {
+        return c*d;
+    }
+
+    public int geteilt(int e, int f) {
+        return e/f;
+    }
 }
