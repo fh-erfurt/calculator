155005ab73924528212c189189376a7c71aaa748
Dwj27 <mail@jkloss.de>
2018-07-06 19:32:23 +0200

    Finally

--- src/main/java/de/fherfurt/git/Calculator.java
+++ src/main/java/de/fherfurt/git/Calculator.java
@@ -2,4 +2,28 @@ package de.fherfurt.git;
 
 public class Calculator {
 
+    public int plus(int _Number1, int _Number2)
+    {
+        int Result = _Number1 + _Number2;
+        return Result;
+    }
+
+    public int minus(int _Number1, int _Number2)
+    {
+        int Result = _Number1 - _Number2;
+        return Result;
+    }
+
+    public int mal(int _Number1, int _Number2)
+    {
+        int Result = _Number1 * _Number2;
+        return Result;
+    }
+
+    public int geteilt(int _Number1, int _Number2)
+    {
+        int Result = _Number1 / _Number2;
+        return Result;
+    }
+
 }
