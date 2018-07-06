12d9322be41833254d622df2852f31ebe1ea65c0
Abrahalhabachi <33718367+Abrahalhabachi@users.noreply.github.com>
2018-07-06 19:37:27 +0200

    all

--- src/main/java/de/fherfurt/git/Calculator.java
+++ src/main/java/de/fherfurt/git/Calculator.java
@@ -4,8 +4,19 @@ package de.fherfurt.git;
 public class Calculator {
 	
 	public int plus(int a, int b) {
-	//Scanner scan = new Scanner();
 	return a+b;
 	}
+	
+	public int minus(int a, int b) {
+		return a-b;
+		}
 
+	public int mal(int a, int b) {
+		return a*b;
+		}
+	
+	public int geteilt(int a, int b) {
+		
+		return a/b;
+		}
 }
--- src/test/java/de/fherfurt/git/CalculatorTest.java
+++ src/test/java/de/fherfurt/git/CalculatorTest.java
@@ -21,17 +21,17 @@ public class CalculatorTest {
     
     @Test
 	public void minus() {
-		fail("Not yet implemented");
+    	assertEquals(3, calculator.minus(5, 2));
 	}
     
     @Test
 	public void mal() {
-		fail("Not yet implemented");
+    	assertEquals(2, calculator.mal(2, 1));
 	}
     
     @Test
 	public void geteilt() {
-		fail("Not yet implemented");
+    	assertEquals(2, calculator.geteilt(2, 1));
 	}
 
 }
