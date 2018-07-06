3725b243ea02856763b672161b73045d9fbf60e3
Michel Rennert <michel.rennert@fh-erfurt.de>
2018-07-06 19:19:10 +0200

    methods and tests are added

--- src/main/java/de/fherfurt/git/Calculator.java
+++ src/main/java/de/fherfurt/git/Calculator.java
@@ -2,4 +2,28 @@ package de.fherfurt.git;
 
 public class Calculator {
 
+    public int plus(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int minus(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int mal(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int geteilt(int a, int b)
+    {
+        if (b == 0)
+            b = 1;
+
+        return a / b;
+    }
+
+
 }
