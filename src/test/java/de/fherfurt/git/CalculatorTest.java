15938712a897c90d7aebfc15a14edea556c5cb3e
an8914be <andrej.bespalov@fh-erfurt.de>
2018-07-06 19:05:01 +0200

    Fertig voll toll

--- src/test/java/de/fherfurt/git/CalculatorTest.java
+++ src/test/java/de/fherfurt/git/CalculatorTest.java
@@ -6,32 +6,41 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
-	
-    private Calculator calculator;
 
-    @Before
-    public void setUp() {
-        calculator = new Calculator();
-    }
+	private Calculator calculator;
 
-    @Test
-	public void plus() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		calculator = new Calculator();
 	}
-    
-    @Test
-	public void minus() {
-		fail("Not yet implemented");
+
+	@Test
+	public double plus(double i, double j)
+	{
+		return i+j;
 	}
-    
-    @Test
-	public void mal() {
-		fail("Not yet implemented");
+
+	@Test
+	public double minus(double i, double j) {
+
+		return i-j;
 	}
-    
-    @Test
-	public void geteilt() {
-		fail("Not yet implemented");
+
+	@Test
+	public double mal(double i, double j) {
+		return i*j;
+	}
+
+	@Test
+	public double geteilt(double i, double j)
+	{
+		if (j != 0)
+		{
+			return i/j;
+		}
+		else{
+			return null;
+		}
 	}
 
 }
